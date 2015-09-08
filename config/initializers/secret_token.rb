cdaa6712271ed898ebb57d79e76baf17b21fb7ce
Alvaro Molina <alvaromolinac@gmail.com>
2015-09-08 17:56:54 -0400

    configure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackeando::Application.config.secret_key_base = '54dc1952fc51929321ed95c88f9e5f592cb48aa3e4d815427a603b581c89ea4f6cc780dccce7d74b62cc3004300844dae85ca92fd47c17e031e2ea15366f5df1'
